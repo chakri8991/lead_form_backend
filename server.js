4ffa87fa45ed818031596d78c5f25ae04c07eef6
chakri8991 <chakravarthi1223@gmail.com>
2025-05-27 23:04:19 +0530

    backend code

---
+++ server.js
@@ -0,0 +1,34 @@
+const express=require("express");
+const app=express();
+const cors=require('cors')
+
+
+app.use(cors())
+app.use(express.json())
+app.listen(5000,()=>{
+    console.log("Server is running on port 5000")
+})
+
+app.post("/",async(req,res)=>{
+    const {name,email,company,message}=req.body;
+    const n8nUrl="http://localhost:5678/webhook-test/webhook"
+    let options={
+        method:"POST",
+        Headers:{
+            "Content-Type":"application/json",
+        },
+        body:JSON.stringify({name,email,company,message})
+
+    }
+
+    try {
+        await fetch(n8nUrl,options)
+    }catch(error){
+        console.log("Failed to forward to n8n :",error.message)
+    }
+    res.send("Lead received and forward to n8n");
+})
+
+
+
+
